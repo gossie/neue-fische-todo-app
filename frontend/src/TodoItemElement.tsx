0b0519cfff74741c49e5234c756ac78bb4d3889a
André Schreck <andre.schreck@gmail.com>
2021-10-20 09:05:05 +0200

    items can be changed via UI

--- frontend/src/TodoItemElement.tsx
+++ frontend/src/TodoItemElement.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TodoItem, TodoList } from "./model";
 import './TodoItemElement.css';
 
@@ -7,6 +8,9 @@ interface TodoItemProps {
 }
 
 export default function TodoItemElement(props: TodoItemProps) {
+
+    const [editMode, setEditMode] = useState(false);
+    const [newTitle, setNewTitle] = useState(props.item.title);
 
     const changeStatus = (status: string) => {
         const link = props.item.links.find(link => link.rel === 'self')?.href
@@ -25,9 +29,36 @@ export default function TodoItemElement(props: TodoItemProps) {
         .then((json: TodoList) => props.todoListEmitter(json));
     };
 
+    const changeTitle = () => {
+        const link = props.item.links.find(link => link.rel === 'self')?.href
+        fetch(`${process.env.REACT_APP_BASE_URL}${link}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title: newTitle,
+                status: props.item.status
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then((json: TodoList) => {
+            setEditMode(false);
+            props.todoListEmitter(json);
+        });
+    };
+
     return (
         <li>
-            <span className={`${props.item.status === 'DONE' ? 'checked' : ''}`}>{props.item.title}</span>
+            {
+                editMode
+                ?
+                    <input type="text" value={newTitle} onChange={(evt) => setNewTitle(evt.target.value)} onKeyUp={(evt) => {if (evt.keyCode === 13) changeTitle();}} />
+                :
+                    <span className={`${props.item.status === 'DONE' ? 'checked' : ''}`} onClick={() => setEditMode(true)}>{props.item.title}</span>
+            }
+            
             {
                 props.item.status === 'OPEN' ? <button onClick={() => changeStatus('DONE')}>Abhaken</button> : <button onClick={() => changeStatus('OPEN')}>Doch nicht</button> 
             }
